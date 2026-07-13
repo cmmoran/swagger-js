e4cd1aee8f0f0fb36fd6d6a5176df2b808e485bd
Chris Moran <chris@chrismoran.com>
2026-07-13 07:45:31 -0400

    build(oas32): include generated package outputs

---
+++ es/helpers/each-operation.js
@@ -0,0 +1,61 @@
+import { isOpenAPI32 } from './openapi-predicates.js';
+
+// iterate over each operation, and fire a callback with details
+// `find=true` will stop iterating, when the cb returns truthy
+export default function eachOperation(spec, cb, find) {
+  if (!spec || typeof spec !== 'object' || !spec.paths || typeof spec.paths !== 'object') {
+    return null;
+  }
+  const {
+    paths
+  } = spec;
+  const supportsAdditionalOperations = isOpenAPI32(spec);
+
+  // Iterate over the spec, collecting operations
+  // eslint-disable-next-line no-restricted-syntax, guard-for-in
+  for (const pathName in paths) {
+    // eslint-disable-next-line no-restricted-syntax, guard-for-in
+    for (const method in paths[pathName]) {
+      if (method.toUpperCase() === 'PARAMETERS' || method === 'additionalOperations') {
+        continue; // eslint-disable-line no-continue
+      }
+      const operation = paths[pathName][method];
+      if (!operation || typeof operation !== 'object') {
+        continue; // eslint-disable-line no-continue
+      }
+      const operationObj = {
+        spec,
+        pathName,
+        method: method.toUpperCase(),
+        operation
+      };
+      const cbValue = cb(operationObj);
+      if (find && cbValue) {
+        return operationObj;
+      }
+    }
+    const {
+      additionalOperations
+    } = paths[pathName];
+    if (supportsAdditionalOperations && additionalOperations && typeof additionalOperations === 'object') {
+      // eslint-disable-next-line no-restricted-syntax, guard-for-in
+      for (const method in additionalOperations) {
+        const operation = additionalOperations[method];
+        if (!operation || typeof operation !== 'object') {
+          continue; // eslint-disable-line no-continue
+        }
+        const operationObj = {
+          spec,
+          pathName,
+          method,
+          operation
+        };
+        const cbValue = cb(operationObj);
+        if (find && cbValue) {
+          return operationObj;
+        }
+      }
+    }
+  }
+  return undefined;
+}
---
+++ es/helpers/id-from-path-method/index.js
@@ -0,0 +1,13 @@
+import replaceSpecialCharsWithUnderscore from '../replace-special-chars-with-underscore.js';
+const STANDARD_HTTP_METHODS = new Set(['connect', 'delete', 'get', 'head', 'options', 'patch', 'post', 'put', 'query', 'trace']);
+export default function idFromPathMethod(pathName, method, {
+  v2OperationIdCompatibilityMode
+} = {}) {
+  if (v2OperationIdCompatibilityMode) {
+    let res = `${method.toLowerCase()}_${pathName}`.replace(/[\s!@#$%^&*()_+=[{\]};:<>|./?,\\'""-]/g, '_');
+    res = res || `${pathName.substring(1)}_${method}`;
+    return res.replace(/((_){2,})/g, '_').replace(/^(_)*/g, '').replace(/([_])*$/g, '');
+  }
+  const normalizedMethod = STANDARD_HTTP_METHODS.has(method.toLowerCase()) ? method.toLowerCase() : method;
+  return `${normalizedMethod}${replaceSpecialCharsWithUnderscore(pathName)}`;
+}
